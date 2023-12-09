ac287d4ed54279005af86062eed27aa0f1a421e5
Jo hanbin <whgksqls9999@naver.com>
2023-12-09 17:07:01 +0900

    feat: info tab 선택 시 해당 내용 출력

--- src/component/info/InfoDetailTab.js
+++ src/component/info/InfoDetailTab.js
@@ -1,9 +1,28 @@
+import "../../App.css";
+import { useEffect, useState } from "react";
+import InfoDetailContent from "./InfoDetailContent";
+
 export default function InfoDetailTab() {
+  let buttons = document.querySelectorAll(".info-detail-tab-button");
+  let [selected, setSelected] = useState(0);
+
+  useEffect(() => {
+    buttons.forEach((element) => element.classList.toggle("selected-tab"));
+    buttons.forEach((element, i) =>
+      element.addEventListener("click", () => {
+        setSelected(i);
+      })
+    );
+  }, [selected]);
+
   return (
-    <div className="info-detail-tab">
-      <div>SKILLS</div>
-      <div>EDUCATION</div>
-      <div>INTRODUCE</div>
-    </div>
+    <>
+      <div className="info-detail-tab">
+        <div className="info-detail-tab-button selected-tab">SKILLS</div>
+        <div className="info-detail-tab-button ">EDUCATION</div>
+        <div className="info-detail-tab-button ">INTRODUCE</div>
+      </div>
+      <InfoDetailContent selectedTab={selected} />
+    </>
   );
 }
