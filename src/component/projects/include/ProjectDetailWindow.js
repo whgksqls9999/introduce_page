da43011e5c53694987ded8b3a787f71b9e17b0ee
Jo hanbin <whgksqls9999@naver.com>
2023-12-21 13:03:44 +0900

    feat: 프로젝트 상세창 페이지 별 스크롤 적용

--- src/component/projects/include/ProjectDetailWindow.js
+++ src/component/projects/include/ProjectDetailWindow.js
@@ -5,15 +5,22 @@ import ProjectDetailWindowHabing from "./ProjectDetailWindowHabing";
 import React from "react";
 
 const ProjectDetailWindow = React.memo(() => {
-  // function ProjectDetailWindow() {
   let [page, setPage] = useState(0);
   let openedProject = useSelector((state) => state.openedProject);
-  // console.log(openedProject);
 
   const project = projects[openedProject];
   const pages = project.content;
   const lastPage = pages.length - 1;
   const progressBtn = useRef([]);
+  const contentWindow = useRef(null);
+
+  function makeScrollable() {
+    contentWindow.current.classList.add("scrollable");
+  }
+
+  function makeNonScrollable() {
+    contentWindow.current.classList.remove("scrollable");
+  }
 
   return (
     <div className="project-detail-window">
@@ -39,8 +46,14 @@ const ProjectDetailWindow = React.memo(() => {
           깃허브
         </a>
       </div>
-      <div className="project-detail-window-content">
-        {openedProject == 0 ? <ProjectDetailWindowHabing page={page} /> : null}
+      <div className="project-detail-window-content" ref={contentWindow}>
+        {openedProject == 0 ? (
+          <ProjectDetailWindowHabing
+            page={page}
+            makeScrollable={makeScrollable}
+            makeNonScrollable={makeNonScrollable}
+          />
+        ) : null}
       </div>
     </div>
   );
--- src/component/projects/include/ProjectDetailWindowHabing.js
+++ src/component/projects/include/ProjectDetailWindowHabing.js
@@ -3,25 +3,54 @@ import projects from "../../../data/ProjectsData";
 import React from "react";
 
 const ProjectDetailWindowHabing = React.memo((props) => {
-  // function ProjectDetailWindowHabing(props) {
   let project = projects[0];
   let curPage = props.page;
 
   const content = useRef(null);
   const page = useRef(null);
 
-  useEffect(() => {
-    // content.current.style.marginTop = `${
-    //   -curPage * page.current.offsetHeight
-    // }px`;
-  }, [curPage]);
+  const makeScrollable = props.makeScrollable;
+  const makeNonScrollable = props.makeNonScrollable;
 
   return (
     <>
       {curPage === 0 ? (
-        <ProjectDetailWindowHabingPage1 page={page} project={project} />
+        <ProjectDetailWindowHabingPage1
+          page={page}
+          project={project}
+          makeNonScrollable={makeNonScrollable}
+        />
       ) : null}
-      {curPage === 1 ? <ProjectDetailWindowHabingPage2 page={page} /> : null}
+      {curPage === 1 ? (
+        <ProjectDetailWindowHabingPage2
+          page={page}
+          makeScrollable={makeScrollable}
+        />
+      ) : null}
+      {curPage === 2 ? (
+        <ProjectDetailWindowHabingPage3
+          page={page}
+          makeScrollable={makeScrollable}
+        />
+      ) : null}
+      {curPage === 3 ? (
+        <ProjectDetailWindowHabingPage4
+          page={page}
+          makeScrollable={makeScrollable}
+        />
+      ) : null}
+      {curPage === 4 ? (
+        <ProjectDetailWindowHabingPage5
+          page={page}
+          makeScrollable={makeScrollable}
+        />
+      ) : null}
+      {curPage === 5 ? (
+        <ProjectDetailWindowHabingPage6
+          page={page}
+          makeNonScrollable={makeNonScrollable}
+        />
+      ) : null}
     </>
   );
 });
@@ -29,6 +58,12 @@ const ProjectDetailWindowHabing = React.memo((props) => {
 function ProjectDetailWindowHabingPage1(props) {
   let page = props.page;
   let project = props.project;
+
+  const makeNonScrollable = props.makeNonScrollable;
+
+  useEffect(() => {
+    makeNonScrollable();
+  }, []);
 
   return (
     <div className="project-detail-window-habing-page" ref={page}>
@@ -75,9 +110,69 @@ function ProjectDetailWindowHabingPage1(props) {
 
 function ProjectDetailWindowHabingPage2(props) {
   let page = props.page;
+  const makeScrollable = props.makeScrollable;
+
+  useEffect(() => {
+    makeScrollable();
+  }, []);
   return (
     <div className="project-detail-window-habing-page" ref={page}>
       <h2>스크롤 애니메이션</h2>
+    </div>
+  );
+}
+
+function ProjectDetailWindowHabingPage3(props) {
+  let page = props.page;
+  const makeScrollable = props.makeScrollable;
+
+  useEffect(() => {
+    makeScrollable();
+  }, []);
+  return (
+    <div className="project-detail-window-habing-page" ref={page}>
+      <h2>카카오맵 API 활용 위치정보 </h2>
+    </div>
+  );
+}
+function ProjectDetailWindowHabingPage4(props) {
+  let page = props.page;
+  const makeScrollable = props.makeScrollable;
+
+  useEffect(() => {
+    makeScrollable();
+  }, []);
+  return (
+    <div className="project-detail-window-habing-page" ref={page}>
+      <h2>로그인/로그아웃</h2>
+    </div>
+  );
+}
+
+function ProjectDetailWindowHabingPage5(props) {
+  let page = props.page;
+  const makeScrollable = props.makeScrollable;
+
+  useEffect(() => {
+    makeScrollable();
+  }, []);
+  return (
+    <div className="project-detail-window-habing-page" ref={page}>
+      <h2>게시글, 장소 좋아요 / 유저간 팔로잉</h2>
+    </div>
+  );
+}
+
+function ProjectDetailWindowHabingPage6(props) {
+  let page = props.page;
+  const makeNonScrollable = props.makeNonScrollable;
+
+  useEffect(() => {
+    makeNonScrollable();
+  }, []);
+  return (
+    <div className="project-detail-window-habing-page" ref={page}>
+      <h2>정리</h2>
     </div>
   );
 }
